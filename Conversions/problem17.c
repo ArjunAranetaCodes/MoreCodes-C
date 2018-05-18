2db0a242d1f3022ebb6c0e8601acc6775daf3f24
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-18 09:56:48 +0800

    2018 Updates

---
+++ Conversions/problem17.c
@@ -0,0 +1,11 @@
+//Problem 17: Write a program that converts kilometer to meter.
+#include <stdio.h>
+
+int main(){
+ int kilometer = 1;
+ int meter = kilometer * 1000;
+ 
+ printf("%d", meter);
+
+ return 0;
+}
