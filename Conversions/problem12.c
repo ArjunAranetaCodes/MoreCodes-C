cdc22313d5c62b471823107172b0fa5932b3cf50
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-18 09:55:43 +0800

    2018 Updates

---
+++ Conversions/problem12.c
@@ -0,0 +1,11 @@
+//Problem 12: Write a program that converts numbers of day to seconds.
+#include <stdio.h>
+
+int main(){
+ int day = 1;
+ int seconds = day * 24 * 60 * 60;
+ 
+ printf("%d", seconds);
+
+ return 0;
+}
