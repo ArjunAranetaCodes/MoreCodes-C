761a55539e86c92c600ff6687b540ccfa04edbff
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-18 09:55:27 +0800

    2018 Updates

---
+++ Conversions/problem11.c
@@ -0,0 +1,11 @@
+//Problem 11: Write a program that converts numbers of day to minutes.
+#include <stdio.h>
+
+int main(){
+ int day = 1;
+ int minutes = day * 24 * 60;
+ 
+ printf("%d", minutes);
+
+ return 0;
+}
