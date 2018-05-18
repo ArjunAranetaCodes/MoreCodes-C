77b969c1f9f3d25deda9db21b1b5174999516f83
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-18 09:57:00 +0800

    2018 Updates

---
+++ Conversions/problem18.c
@@ -0,0 +1,11 @@
+//Problem 18: Write a program that converts liter to milliliter.
+#include <stdio.h>
+
+int main(){
+ int liter = 1;
+ int milliliter = liter * 1000;
+ 
+ printf("%d", milliliter);
+
+ return 0;
+}
