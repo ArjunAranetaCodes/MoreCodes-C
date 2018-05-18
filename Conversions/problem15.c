f4c9b7b3eaea0178414aa30835ada7d0eea768d8
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-18 09:56:23 +0800

    2018 Updates

---
+++ Conversions/problem15.c
@@ -0,0 +1,12 @@
+//Problem 15: Write a program that converts letters to ASCII.
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+ char letter = 'x';
+ int ascii = (int)letter;
+ 
+ printf("%d", ascii);
+
+ return 0;
+}
